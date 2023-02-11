9efb4abc82ffcc54d8bd092d0f850c987d9c3395
dominikdziuba <56097555+dominikdziuba@users.noreply.github.com>
2023-02-11 02:20:44 +0100

    Repairing things

--- lab9/src/fireStation/Observer.java
+++
@@ -1,8 +0,0 @@
-package fireStation;
-
-import incidents.Incident;
-
-public interface Observer {
-    public void update(Incident event, int counterOfCars);
-
-}
--- lab9/src/types/LocalDanger.java
+++
@@ -1,9 +0,0 @@
-package types;
-
-public class LocalDanger implements IType{
-
-    @Override
-    public boolean action() {
-        return false;
-    }
-}
